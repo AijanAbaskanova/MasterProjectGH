d7ae26a43fdcd2e344fe2a16cd0e3277bc2e6ab6
AijanAbaskanova <abaskanova@gmail.com>
2025-04-12 21:56:34 -0500

    Java task

--- src/main/java/Math_Descret/Task3.java
+++ src/main/java/Math_Descret/Task3.java
@@ -5,17 +5,16 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
-        Scanner sc =  new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int d = sc.nextInt();
 
-        for (int i = a; i <= b; i++){
-            if (i % d == c){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        int d = scan.nextInt();
+        for (int i = a; i <= b; i++) {
+            if (i % d == c) {
                 System.out.println(i);
             }
         }
-
     }
 }
