4997c8cd9371de98baca8bb9653b9545e1679c9a
AijanAbaskanova <abaskanova@gmail.com>
2025-04-13 20:31:53 -0500

    Even number

--- src/main/java/Java/Arrays/EvenNumbers.java
+++ src/main/java/Java/Arrays/EvenNumbers.java
@@ -3,24 +3,24 @@ package Java.Arrays;
 import java.util.Scanner;
 
 public class EvenNumbers {
-
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
         int[] array = new int[n];
 
-
         for (int i = 0; i < n; i++) {
             array[i] = scanner.nextInt();
         }
 
-
         for (int i = 0; i < n; i += 2) {
-            System.out.print(array[i] + " ");
+            if (i > 0) {
+                System.out.print(" ");
+            }
+            System.out.print(array[i]);
         }
     }
 }
+
 
 
